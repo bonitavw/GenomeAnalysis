feec39a32a45218439285ad01aa09d4bd3e513e0
Bonita van Waardenburg <bonitavw@rackham3.uppmax.uu.se>
2023-04-08 15:36:37 +0200

    tried genome assembly on pacbio data, still errors

--- code/02_genome_assembly/sbatch_ga_canu.sh
+++ code/02_genome_assembly/sbatch_ga_canu.sh
@@ -3,17 +3,16 @@
 #SBATCH -M snowy
 #SBATCH -p core
 #SBATCH -n 2
-#SBATCH -t 04:00:00
-#SBATCH -J ga-spades
-#SBATCH -o /domus/h1/bonitavw/GenomeAnalysis/logs/ga-spades.log
+#SBATCH -t 05:00:00
+#SBATCH -J ga-canu
+#SBATCH -o /domus/h1/bonitavw/GenomeAnalysis/logs/ga-canu.log
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user vanwaardenburgbonita@gmail.com
 
-TRIM_DATA="/domus/h1/bonitavw/GenomeAnalysis/data/trimmed_data/genomics_data/Illumina"
+PACBIO_DATA="/domus/h1/bonitavw/GenomeAnalysis/data/raw_data/genomics_data/PacBio"
 ANALYSIS_DIR="/domus/h1/bonitavw/GenomeAnalysis/analysis"
 
 module load bioinfo-tools
-module load spades
+module load canu
 
-spades.py --pe1-1 ${TRIM_DATA}/E745-1.L500_SZAXPI015146-56_1P_clean.fq.gz --pe1-2 ${TRIM_DATA}/E745-1.L500_SZAXPI015146-56_2P_clean.fq.gz \
-    --isolate -o ${ANALYSIS_DIR}/02_genome_assembly/spades_output 
+canu -p 'canu-PacBio' -d ${ANALUSIS_DIR}/02_genome_assembly/canu_output/ -pacbio-raw ${PACBIO}/*fastq.gz
